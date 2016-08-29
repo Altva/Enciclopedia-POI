c2d975addcc9250950564358fb663060a6da7f1c
iMaximuz@gmail.com <imaximuz@gmail.com>
2016-08-28 23:36:29 -0500

    poder cargar contenidos de otros html

---
+++ js/conentload.js
@@ -0,0 +1,6 @@
+
+var LoadContent = function(bar){
+    alert(bar);
+    var content = document.getElementById("content");
+    content.innerHTML = '<object type="text/html" data="temas/tema1.html" ></object>';
+}
